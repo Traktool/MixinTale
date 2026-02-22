8e8a9c9991800b04b5d556e153adee310b549f69
Traktool <trak-tool@hotmail.fr>
2026-02-22 11:00:56 +0100

    Build shaded processor jar with bundled Gson

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
     `java-library`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 dependencies {
     implementation(project(":api"))
     implementation("com.google.code.gson:gson:2.11.0")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+}
+
+tasks.jar {
+    archiveClassifier.set("slim")
 }
